8615f230ee6c9e99d7aaf3867ae7be956ab31672
Damian Kaczmarek <rush@rushbase.net>
2023-05-07 16:59:56 -0500

    Bump version

--- src/Addon.h
+++ src/Addon.h
@@ -136,9 +136,13 @@ class NativeString {
       Local<ArrayBufferView> arrayBufferView =
           Local<ArrayBufferView>::Cast(value);
 #if NODE_MAJOR_VERSION >= 18
-      std::shared_ptr<v8::BackingStore> contents = arrayBufferView->Buffer()->GetBackingStore();
+      // std::shared_ptr<v8::BackingStore> contents = arrayBufferView->Buffer()->GetBackingStore();
+      // length = arrayBufferView->ByteLength();
+      // data = (char *)contents->Data();
+       Local<ArrayBufferView> arrayBufferView =
+          Local<ArrayBufferView>::Cast(value);
       length = arrayBufferView->ByteLength();
-      data = (char *)contents->Data();
+      data = reinterpret_cast<char*>(arrayBufferView->Buffer()->GetContents().Data());
 #else
       ArrayBuffer::Contents contents = arrayBufferView->Buffer()->GetContents();
       length = contents.ByteLength();
@@ -147,9 +151,12 @@ class NativeString {
     } else if (value->IsArrayBuffer()) {
       Local<ArrayBuffer> arrayBuffer = Local<ArrayBuffer>::Cast(value);
 #if NODE_MAJOR_VERSION >= 18
-      std::shared_ptr<v8::BackingStore> contents = arrayBuffer->GetBackingStore();
+      // std::shared_ptr<v8::BackingStore> contents = arrayBuffer->GetBackingStore();
+      // length = arrayBuffer->ByteLength();
+      // data = (char *)contents->Data();
+      Local<ArrayBuffer> arrayBuffer = Local<ArrayBuffer>::Cast(value);
       length = arrayBuffer->ByteLength();
-      data = (char *)contents->Data();
+      data = reinterpret_cast<char*>(arrayBuffer->GetContents().Data());
 #else
       ArrayBuffer::Contents contents = arrayBuffer->GetContents();
       length = contents.ByteLength();
